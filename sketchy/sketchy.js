5ea695d8e7a3186b55a0e4f5b8d2489ad3220662
charlotte <83621359+charlottechiodo@users.noreply.github.com>
2021-06-04 16:01:01 +1000

    Update sketchy.js

--- sketchy/sketchy.js
+++ sketchy/sketchy.js
@@ -1,108 +1,143 @@
-// Acknowledgement to 'Vehicle class & Steering behaviour' by Daniel Shiffman
+// Vehicle class & Steering behaviour code by Daniel Shiffman
 // http://codingtra.in
 // Steering Text Paths Video: https://www.youtube.com/watch?v=4hA7G3gup-4
 
-var font;
-var vehicles = [];
-var Vehicle;
-
-function Vehicle(x,y) {
-this.pos = createVector(random(width), random(height)); //starting position
-this.target = createVector(x,y); //target position
-this.vel = p5.Vector.random2D(); //velocity
-this.acc = createVector(); //acceleration
-this.r = 8;
-this.maxspeed = 15; //desired velocity
-this.maxforce = 1; //how good it is at steering and returning back to original position
-}
-
-Vehicle.prototype.behaviors = function() { //multiple behaviours at play, accumulates a bunch of behaviours
- var arrive = this.arrive(this.target);
- var mouse = createVector(mouseX,mouseY);
- var flee = this.flee(mouse);
-
- arrive.mult(1);
- flee.mult(5); //fleeing force is much stronger than arriving force
- 
- this.applyForce(arrive);
- this.applyForce(flee);
-};
-
-Vehicle.prototype.applyForce = function(f) { // f = force, add multiple forces into acceleration
-this.acc.add(f);
-};
-
-Vehicle.prototype.update = function() {
-this.pos.add(this.vel); //velocity changing position
-this.vel.add(this.acc); //acceleration changing velocity
-this.acc.mult(0); //clear acceleration
-};
-
-Vehicle.prototype.show = function() {
-stroke(255);
-strokeWeight(8);
-point(this.pos.x, this.pos.y);
-};
-
-Vehicle.prototype.arrive = function(target) {
-  var desired = p5.Vector.sub(target, this.pos); //get a vector that points from position to target 
-  var d = desired.mag();
-  var speed = this.maxspeed;
-  if (d < 100) { //if distance is less than 100
-  speed = map(d, 0, 100, 0, this.maxspeed); //when distance is 0, mag is 0, when distance is 100, magnitude is maxspeed
-  } 
-  desired.setMag(speed); //magnitude
- var steer = p5.Vector.sub(desired, this.vel); //steering force, desired speed minus velocity
- steer.limit(this.maxforce); //steering force wont be as strong
- return steer;
-};
-
-
-Vehicle.prototype.flee = function(target) { //THE DOTS FLEE, MOVE IN OPPOSITE DIRECTION OF MOUSE
-  var desired = p5.Vector.sub(target, this.pos); //get a vector that points from position to target 
-  var d = desired.mag();
-  if (d < 50) {
-  desired.setMag(this.maxspeed);
-  desired.mult(-1); //multiply the force in teh opposite direction
- var steer = p5.Vector.sub(desired, this.vel); //steering force, desired speed minus velocity
- steer.limit(this.maxforce); //steering force wont be as strong
- return steer;
-} else {
- return createVector(0,0);
- }
-};
-
+var font, points_array,points_array2, ballSize, words;
+var myText ='LALIQUE ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890';
+var points1 = [];
+var points2 = [];
+var options={ sampleFactor: 0.2,simplifyThreshold: 0};
+var splash=true;
 function preload() {
-font = loadFont('data/Lalique.otf');
+  font = loadFont('data/Lalique.otf');
 }
 
 function setup() {
-createCanvas(windowWidth, windowHeight);
-background(0);
-
-//instead of drawing it as text, want to get the points/vertices of the letters
-
-var points = font.textToPoints('LALIQUE', 10, 450, 200); //last is fontsize
-
-for (var i = 0; i < points.length; i++){
-var pt = points[i];
-var vehicle = new Vehicle(pt.x, pt.y);
-vehicles.push(vehicle); // making a vehicle for every point
+  createCanvas(windowWidth, windowHeight);
+  fontSize=width/20;
+  textSize(fontSize);
+  //textSpacing(-fontSize/2);
+  //textAlign(CENTER);
+  background(0);
+  words=myText.split(' ');
+  textLeading(32*1.5);
+  //let textBox = font.textBounds('run your mouse', 0, 0, fontSize);
+  points_array = font.textToPoints('run your mouse over', 0, height/6, fontSize, options);  
+  for(let i = 0; i < points_array.length; i++) {
+    let point = points_array[i];
+    let vehicle = new Vehicle(point.x, point.y, 4);
+    points1.push(vehicle);
+  }
 }
-}
-
 
 function draw() {
-background(0);
-for (var i = 0; i < vehicles.length; i++) {
-var v = vehicles[i];
-v.behaviors();
-v.update();
-v.show();
-fill(255);
-noStroke();
-textSize(20);
-text('move your mouse through the letters', 55, 215);
-textFont(font);
+  clear();
+  background(51);
+  
+//  for(var i=0; i<words.length;i++) {
+//  text(words[i], 20, i*32);
+//  }
+  
+  for(let i = 0; i < points1.length; i++) {
+    let point = points1[i];
+    point.update();
+    point.behaviors();
+    point.show();
+  }
+  if (splash==true) {
+    if (frameCount>250){
+      splash=!splash;
+      addLalique();
+      } 
+   }
 }
+function addLalique(){
+  fontSize=30;
+    //points1 = [];
+    points_array = font.textToPoints('LALIQUE ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890', width/23, height/2, fontSize, options);  
+  for(let i = 0; i < points_array.length; i++) {
+    let point = points_array[i];
+    let vehicle = new Vehicle(point.x, point.y, 4);
+    points1.push(vehicle);
+  } 
+}
+function Vehicle(x,y, ballSize) {
+  //this.pos = createVector(x,y);
+  this.pos = createVector(random(width), random(height));
+  this.vel = p5.Vector.random2D();
+  this.acc = createVector();
+  this.target = createVector(x,y);
+  this.r = 8;
+  this.maxspeed = 5;
+  this.maxforce = 0.3;
+  this.ballSize=ballSize;
+  
+  this.behaviors = function() {
+    let arrive = this.arrive(this.target);
+    let mouse = createVector(mouseX, mouseY);
+    let flee = this.flee(mouse);
+    
+    arrive.mult(1);
+    flee.mult(5);
+    
+    this.applyForce(arrive);
+    this.applyForce(flee);
+  }
+  
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+  
+  this.arrive = function(target) {
+    let desired = p5.Vector.sub(target, this.pos);
+    let distance = desired.mag();
+    let speed = this.maxspeed;
+    if ( distance < 300 ) {
+      speed = map(distance, 0, 100, 0, this.maxspeed);
+    } 
+    
+    desired.setMag(speed);
+    var steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxforce);
+    
+    return steer;
+  }
+  
+  this.flee = function(target) {
+    let desired = p5.Vector.sub(target, this.pos);
+    let distance = desired.mag();
+    if ( distance < 50 ) {
+      desired.setMag(this.maxspeed);
+      desired.mult(-1);
+      var steer = p5.Vector.sub(desired, this.vel);
+      steer.limit(this.maxforce);
+
+      return steer;
+    } else {
+      return createVector(0, 0);
+    }
+  }
+  
+  this.update = function() {
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.mult(0);
+  }
+  
+  this.show = function() {
+    stroke(255);
+    strokeWeight(this.ballSize);
+    point(this.pos.x, this.pos.y);
+  }
+}
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+  points1=[];
+  fontsize=width/20;
+  points_array = font.textToPoints('run your mouse over', 0, height/6, fontSize, options);  
+  for(let i = 0; i < points_array.length; i++) {
+    let point = points_array[i];
+    let vehicle = new Vehicle(point.x, point.y, 4);
+    points1.push(vehicle);
+  }
 }
