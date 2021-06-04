0cf53b1031761cfef5b83570cb058f692fb12008
charlotte <83621359+charlottechiodo@users.noreply.github.com>
2021-06-04 17:35:38 +1000

    Update sketchy.js

--- sketchy/sketchy.js
+++ sketchy/sketchy.js
@@ -2,8 +2,7 @@
 // http://codingtra.in
 // Steering Text Paths Video: https://www.youtube.com/watch?v=4hA7G3gup-4
 
-var font, points_array,points_array2, ballSize, words;
-var myText ='LALIQUE ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890';
+var font, points_array,points_array2, ballSize;
 var points1 = [];
 var points2 = [];
 var options={ sampleFactor: 0.2,simplifyThreshold: 0};
@@ -11,18 +10,16 @@ var splash=true;
 function preload() {
   font = loadFont('data/Lalique.otf');
 }
+//instead of drawing it as text, want to get the points/vertices of the letters
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fontSize=width/20;
+  fontSize=35;
   textSize(fontSize);
   //textSpacing(-fontSize/2);
   //textAlign(CENTER);
   background(0);
-  words=myText.split(' ');
-  textLeading(32*1.5);
-  //let textBox = font.textBounds('run your mouse', 0, 0, fontSize);
-  points_array = font.textToPoints('run your mouse over', 0, height/6, fontSize, options);  
+  points_array = font.textToPoints('run your mouse over', width/3.8, height/7, fontSize, options);  
   for(let i = 0; i < points_array.length; i++) {
     let point = points_array[i];
     let vehicle = new Vehicle(point.x, point.y, 4);
@@ -32,12 +29,10 @@ function setup() {
 
 function draw() {
   clear();
-  background(51);
-  
-//  for(var i=0; i<words.length;i++) {
-//  text(words[i], 20, i*32);
-//  }
-  
+  background(0);
+  rect(30,40);
+  fill(20);
+ 
   for(let i = 0; i < points1.length; i++) {
     let point = points1[i];
     point.update();
@@ -48,37 +43,70 @@ function draw() {
     if (frameCount>250){
       splash=!splash;
       addLalique();
+      addAlpha1();
+      addAlpha2();
+      addNumbers();
       } 
    }
 }
+
 function addLalique(){
-  fontSize=30;
-    //points1 = [];
-    points_array = font.textToPoints('LALIQUE ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890', width/23, height/2, fontSize, options);  
+  fontSize=73;
+    points_array = font.textToPoints('LALIQUE', width/3.2, height/3, fontSize, options);  
   for(let i = 0; i < points_array.length; i++) {
     let point = points_array[i];
-    let vehicle = new Vehicle(point.x, point.y, 4);
-    points1.push(vehicle);
-  } 
+   let vehicle = new Vehicle(point.x, point.y, 4);
+   points1.push(vehicle);
+ } 
 }
+
+function addAlpha1(){
+  fontSize=60;
+    points_array = font.textToPoints('ABCDEFGHIJKLM', width/4.5, height/2, fontSize, options);  
+  for(let i = 0; i < points_array.length; i++) {
+    let point = points_array[i];
+   let vehicle = new Vehicle(point.x, point.y, 4);
+   points1.push(vehicle);
+ } 
+}
+
+function addAlpha2(){
+  fontSize=60;
+    points_array = font.textToPoints('NOPQRSTUVWXYZ', width/4.5, height/1.5, fontSize, options);  
+  for(let i = 0; i < points_array.length; i++) {
+    let point = points_array[i];
+   let vehicle = new Vehicle(point.x, point.y, 4);
+   points1.push(vehicle);
+ } 
+}
+
+function addNumbers(){
+  fontSize=60;
+    points_array = font.textToPoints('1234567890', width/3.5, height/1.2, fontSize, options);  
+  for(let i = 0; i < points_array.length; i++) {
+    let point = points_array[i];
+   let vehicle = new Vehicle(point.x, point.y, 4);
+   points1.push(vehicle);
+ } 
+}
+
 function Vehicle(x,y, ballSize) {
-  //this.pos = createVector(x,y);
-  this.pos = createVector(random(width), random(height));
-  this.vel = p5.Vector.random2D();
-  this.acc = createVector();
-  this.target = createVector(x,y);
+  this.pos = createVector(random(width), random(height)); //starting position
+  this.vel = p5.Vector.random2D(); //velocity
+  this.acc = createVector(); //acceleration
+  this.target = createVector(x,y); //target position
   this.r = 8;
-  this.maxspeed = 5;
-  this.maxforce = 0.3;
+  this.maxspeed = 5; //desired velocity
+  this.maxforce = 0.3; //how good it is at steering and returning back to original position
   this.ballSize=ballSize;
   
-  this.behaviors = function() {
+  this.behaviors = function() {//accumulates a bunch of behaviours
     let arrive = this.arrive(this.target);
     let mouse = createVector(mouseX, mouseY);
     let flee = this.flee(mouse);
     
     arrive.mult(1);
-    flee.mult(5);
+    flee.mult(5);//fleeing force is much stronger than arriving force
     
     this.applyForce(arrive);
     this.applyForce(flee);
